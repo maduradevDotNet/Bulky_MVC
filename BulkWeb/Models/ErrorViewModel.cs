40d1e5b9b6dc81e75f294d326667ff8d8370ccc2
pasindu dissanayake <123033890+maduradevDotNet@users.noreply.github.com>
2024-07-01 02:24:02 +0530

    Add project files.

---
+++ BulkWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BulkWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
